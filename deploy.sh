28006956b71e773a1e06260857e618a426612cd3
张殃离 <96040653+ZhangYangLizzm@users.noreply.github.com>
2023-07-08 03:15:10 +0800

    update version

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push git@github.com:ZhangYangLizzm/blog.git master:gh-pages
+git push -f git@github.com:ZhangYangLizzm/blog.git master:gh-pages
 
 cd -
